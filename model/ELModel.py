5c2f02323adea8fd71c3bab2ee372be5ea2c6bc0
XiShao-art <11712611@mail.sustech.edu.cn>
2021-09-08 00:52:06 +0300

    add annotation, set the framework

---
+++ model/ELModel.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class ELModel(nn.Module):
+    def __init__(self):
+        super(ELModel, self).__init__()
+
+        
+
+
---
+++ model/LossFunc.py
@@ -0,0 +1,22 @@
+
+class LossFunc:
+    def __init__(self):
+        pass
+
+    def nf1(self):
+        pass
+
+    def nf2(self):
+        pass
+
+    def nf3(self):
+        pass
+
+    def nf4(self):
+        pass
+
+    def disjoint(self):
+        pass
+
+    def top(self):
+        pass
