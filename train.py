11b595136eb163b3607bc150680c6f06521904f9
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2022-07-08 03:47:59 +0100

    Adds support for training on the EL datasets

---
+++ train.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+import click as ck
+import numpy
+import torch.optim as optim
+from model.ELBoxlModel import ELBoxModel
+from utils.el_data_loader import load_data, load_valid_data
+import logging
+import torch
+import pandas as pd
+import numpy as np
+from tqdm import trange
+import wandb
+
+from utils.utils import get_device
+
+logging.basicConfig(level=logging.INFO)
+
+
+@ck.command()
+# family_normalized.owl
+# yeast-classes-normalized.owl
+@ck.option(
+    '--out-classes-file', '-ocf', default='data/classPPIEmbed.pkl',
+    help='Pandas pkl file with class embeddings')
+@ck.option(
+    '--out-relations-file', '-orf', default='data/relationPPIEmbed.pkl',
+    help='Pandas pkl file with relation embeddings')
+@ck.option(
+    '--batch-size', '-bs', default=512,
+    help='Batch size')
+@ck.option(
+    '--epochs', '-e', default=1000,
+    help='Training epochs')
+@ck.option(
+    '--device', '-d', default='gpu:0',
+    help='GPU Device ID')
+@ck.option(
+    '--embedding-size', '-es', default=50,
+    help='Embeddings size')
+@ck.option(
+    '--reg-norm', '-rn', default=1,
+    help='Regularization norm')
+@ck.option(
+    '--margin', '-m', default=-0.1,
+    help='Loss margin')
+@ck.option(
+    '--learning-rate', '-lr', default=0.01,
+    help='Learning rate')
+@ck.option(
+    '--params-array-index', '-pai', default=-1,
+    help='Params array index')
+@ck.option(
+    '--loss-history-file', '-lhf', default='data/loss_history.csv',
+    help='Pandas pkl file with loss history')
+def main(out_classes_file, out_relations_file,
+         batch_size, epochs, device, embedding_size, reg_norm, margin,
+         learning_rate, params_array_index, loss_history_file):
+    wandb.init(project="el2box-GO", entity="krr")
+
+    device = get_device()
+
+    # training procedure
+    train_data, classes, relations = load_data('GO')
+    print(len(relations))
+    embedding_dim = 50
+    model = ELBoxModel(device, classes, len(relations), embedding_dim=embedding_dim, batch=batch_size, margin1=-0.05)
+
+    #
+    # checkpoint = torch.load('./netPlot.pkl')
+    # model.load_state_dict(checkpoint.state_dict())  # 加载网络权重参数
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    model = model.to(device)
+    train(model, train_data, optimizer, classes, relations)
+    model.eval()
+
+    model = model.to(device)
+
+    cls_file = out_classes_file
+    df = pd.DataFrame(
+        {'classes': list(classes.keys()),
+         'embeddings': list(model.classEmbeddingDict.weight.clone().detach().cpu().numpy())})
+    df.to_pickle(cls_file)
+
+    rel_file = out_relations_file
+    df = pd.DataFrame(
+        {'relations': list(relations.keys()),
+         'embeddings': list(model.relationEmbeddingDict.weight.clone().detach().cpu().numpy())})
+
+    df.to_pickle(rel_file)
+
+
+def train(model, data, optimizer, aclasses, relations, num_epochs=7000):
+    model.train()
+    wandb.watch(model)
+
+    for epoch in trange(num_epochs):
+        re = model(data)
+        loss = sum(re)
+        wandb.log({'loss': loss})
+        if epoch % 1000 == 0:
+            print('epoch:', epoch, 'loss:', round(loss.item(), 3))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+if __name__ == '__main__':
+    main()
--- utils/el_data_loader.py
+++ utils/el_data_loader.py
@@ -1,7 +1,68 @@
 import numpy as np
-np.random.seed(100)
 
-def load_data(filename):
+from utils.utils import get_device
+
+np.random.seed(100)
+import torch
+
+device = get_device()
+
+
+def load_valid_data(valid_data_file, classes, relations):
+    data = []
+    rel = f'SubClassOf'
+    with open(valid_data_file, 'r') as f:
+        for line in f:
+            it = line.strip().split()
+            id1 = it[0]
+            id2 = it[1]
+            if id1 not in classes or id2 not in classes or rel not in relations:
+                continue
+            data.append((classes[id1], relations[rel], classes[id2]))
+    return data
+
+
+def load_eval_data(data_file):
+    data = []
+    rel = f'SubClassOf'
+    with open(data_file, 'r') as f:
+        for line in f:
+            it = line.strip().split()
+            id1 = it[0]
+            id2 = it[1]
+            data.append((id1, id2))
+    return data
+
+
+def load_cls(train_data_file):
+    train_subs = list()
+    counter = 0
+    with open(train_data_file, 'r') as f:
+        for line in f:
+            counter += 1
+            it = line.strip().split()
+            cls1 = it[0]
+            cls2 = it[1]
+            train_subs.append(cls1)
+            train_subs.append(cls2)
+    train_cls = list(set(train_subs))
+    return train_cls, counter
+
+
+def get_all_sub_cls(dataset):
+    train_file = f"data/{dataset}/{dataset}_train.txt"
+    va_file = f"data/{dataset}/{dataset}_valid.txt"
+    test_file = f"data/{dataset}/{dataset}_test.txt"
+    train_sub_cls, train_samples = load_cls(train_file)
+    valid_sub_cls, valid_samples = load_cls(va_file)
+    test_sub_cls, test_samples = load_cls(test_file)
+    total_sub_cls = train_sub_cls + valid_sub_cls + test_sub_cls
+    all_sub_cls = list(set(total_sub_cls))
+    return all_sub_cls
+
+
+def load_data(dataset):
+    filename = f"data/{dataset}/{dataset}_latest_norm_mod.owl"
     classes = {}
     relations = {}
     data = {'nf1': [], 'nf2': [], 'nf3': [], 'nf4': [], 'disjoint': []}
@@ -69,14 +130,14 @@ def load_data(filename):
                     classes[c] = len(classes)
                 if d not in classes:
                     classes[d] = len(classes)
-                data['nf1'].append((classes[c] ,relations[r] ,classes[d]))
+                data['nf1'].append((classes[c], relations[r], classes[d]))
 
     if 'owl:Thing' not in classes:
         classes['owl:Thing'] = len(classes)
 
     prot_ids = []
     class_keys = list(classes.keys())
-    for val in all_subcls:
+    for val in get_all_sub_cls(dataset):
         if val not in class_keys:
             cid = len(classes)
             classes[val] = cid
@@ -84,7 +145,6 @@ def load_data(filename):
         else:
             prot_ids.append(classes[val])
     prot_ids = np.array(prot_ids)
-
 
     # Add corrupted triples nf3
     n_classes = len(classes)
@@ -97,18 +157,16 @@ def load_data(filename):
         y = np.random.choice(prot_ids)
         while y == d:
             y = np.random.choice(prot_ids)
-        data['nf3_neg'].append((c, r ,x))
-        data['nf3_neg'].append((y, r ,d))
+        data['nf3_neg'].append((c, r, x))
+        data['nf3_neg'].append((y, r, d))
 
-
-    data['nf1'] = np.array(data['nf1'])
-    data['nf2'] = np.array(data['nf2'])
-    data['nf3'] = np.array(data['nf3'])
-    data['nf4'] = np.array(data['nf4'])
-    # to avoid empty set
-    data['disjoint'] = np.array([[0 ,0]])
-    data['top'] = np.array([classes['owl:Thing'] ,])
-    data['nf3_neg'] = np.array(data['nf3_neg'])
+    data['nf1'] = torch.tensor(data['nf1'], dtype=torch.int32)[:, [0, 2]]
+    data['nf2'] = torch.tensor(data['nf2'], dtype=torch.int32)
+    data['nf3'] = torch.tensor(data['nf3'], dtype=torch.int32)
+    data['nf4'] = torch.tensor(data['nf4'], dtype=torch.int32)
+    data['disjoint'] = torch.tensor(data['disjoint'], dtype=torch.int32)
+    data['top'] = torch.tensor([classes['owl:Thing']], dtype=torch.int32)
+    data['nf3_neg'] = torch.tensor(data['nf3_neg'], dtype=torch.int32)
 
     for key, val in data.items():
         index = np.arange(len(data[key]))
