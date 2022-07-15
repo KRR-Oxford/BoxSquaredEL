851fe9059e43310a121741fae77145baa3459cea
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2022-07-15 13:40:46 +0100

    Implements validation during training

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import pandas as pd
 import numpy as np
 from tqdm import trange
 import wandb
+from evaluate import compute_ranks
 
 from utils.utils import get_device
 
@@ -17,14 +18,6 @@ logging.basicConfig(level=logging.INFO)
 
 
 @ck.command()
-# family_normalized.owl
-# yeast-classes-normalized.owl
-@ck.option(
-    '--out-classes-file', '-ocf', default='data/classPPIEmbed.pkl',
-    help='Pandas pkl file with class embeddings')
-@ck.option(
-    '--out-relations-file', '-orf', default='data/relationPPIEmbed.pkl',
-    help='Pandas pkl file with relation embeddings')
 @ck.option(
     '--batch-size', '-bs', default=512,
     help='Batch size')
@@ -52,47 +45,39 @@ logging.basicConfig(level=logging.INFO)
 @ck.option(
     '--loss-history-file', '-lhf', default='data/loss_history.csv',
     help='Pandas pkl file with loss history')
-def main(out_classes_file, out_relations_file,
-         batch_size, epochs, device, embedding_size, reg_norm, margin,
+def main(batch_size, epochs, device, embedding_size, reg_norm, margin,
          learning_rate, params_array_index, loss_history_file):
-    wandb.init(project="el2box-GO", entity="krr")
+    dataset = 'ANATOMY'
+    embedding_dim = 50
+    out_classes_file = f'data/{dataset}/classELEmbed'
+    out_relations_file = f'data/{dataset}/relationELEmbed'
+    val_file = f'data/{dataset}/{dataset}_valid.txt'
+
+    wandb.init(project=f"el2box-{dataset}", entity="krr")
 
     device = get_device()
 
     # training procedure
-    train_data, classes, relations = load_data('GO')
-    print(len(relations))
-    embedding_dim = 50
-    model = ELBoxModel(device, classes, len(relations), embedding_dim=embedding_dim, batch=batch_size, margin1=-0.05)
+    train_data, classes, relations = load_data(dataset)
+    val_data = load_valid_data(val_file, classes, relations)
+    print('Loaded data.')
+    model = ELBoxModel(device, classes, len(relations), embedding_dim=embedding_dim, batch=batch_size, margin1=-0.1)  # TODO: margin
 
-    #
-    # checkpoint = torch.load('./netPlot.pkl')
-    # model.load_state_dict(checkpoint.state_dict())  # 加载网络权重参数
 
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=1e-2)
     model = model.to(device)
-    train(model, train_data, optimizer, classes, relations)
-    model.eval()
+    train(model, train_data, val_data, optimizer, out_classes_file, out_relations_file, classes, relations, val_freq=100)
+    # model.eval()
 
-    model = model.to(device)
-
-    cls_file = out_classes_file
-    df = pd.DataFrame(
-        {'classes': list(classes.keys()),
-         'embeddings': list(model.classEmbeddingDict.weight.clone().detach().cpu().numpy())})
-    df.to_pickle(cls_file)
-
-    rel_file = out_relations_file
-    df = pd.DataFrame(
-        {'relations': list(relations.keys()),
-         'embeddings': list(model.relationEmbeddingDict.weight.clone().detach().cpu().numpy())})
-
-    df.to_pickle(rel_file)
+    # model = model.to(device)
+    # save_model(model, out_classes_file, out_relations_file, classes, relations)
 
 
-def train(model, data, optimizer, aclasses, relations, num_epochs=7000):
+def train(model, data, val_data, optimizer, out_classes_file, out_relations_file, classes, relations, num_epochs=7000, val_freq=100):
     model.train()
     wandb.watch(model)
+    best_top100 = 0
+    best_epoch = 0
 
     for epoch in trange(num_epochs):
         re = model(data)
@@ -100,9 +85,32 @@ def train(model, data, optimizer, aclasses, relations, num_epochs=7000):
         wandb.log({'loss': loss})
         if epoch % 1000 == 0:
             print('epoch:', epoch, 'loss:', round(loss.item(), 3))
+        if epoch % val_freq == 0:
+            embeds = model.classEmbeddingDict.weight.clone().detach()
+            top1, top10, top100, mean_rank, ranks = compute_ranks(embeds, model.embedding_dim, val_data[:1000], model.device)
+            wandb.log({'top10': top10, 'top100': top100, 'mean_rank': mean_rank})
+            if top100 > best_top100:
+                best_top100 = top100
+                best_epoch = epoch
+                save_model(model, f'{out_classes_file}_best.pkl', f'{out_relations_file}_best.pkl', classes, relations)
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+    print(f'Best epoch: {best_epoch}')
+
+
+def save_model(model, cls_file, rel_file, classes, relations):
+    df = pd.DataFrame(
+        {'classes': list(classes.keys()),
+         'embeddings': list(model.classEmbeddingDict.weight.clone().detach().cpu().numpy())})
+    df.to_pickle(cls_file)
+
+    df = pd.DataFrame(
+        {'relations': list(relations.keys()),
+         'embeddings': list(model.relationEmbeddingDict.weight.clone().detach().cpu().numpy())})
+    df.to_pickle(rel_file)
 
 
 if __name__ == '__main__':
