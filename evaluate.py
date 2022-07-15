c75ae4318942896aa4fa11733160e45d23fc130c
Mathias Jackermeier <mathias.jackermeier@outlook.de>
2022-07-15 13:53:08 +0100

    Cleanup

--- evaluate.py
+++ evaluate.py
@@ -2,38 +2,61 @@ from collections import Counter
 
 import pandas as pd
 import logging
-from tqdm import trange
 import numpy as np
 import torch
-
-from sklearn.metrics import roc_curve, auc
 import math
+from tqdm import trange
+
 from utils.utils import get_device
 from utils.el_data_loader import load_data, load_valid_data
 
 logging.basicConfig(level=logging.INFO)
 
 
-def compute_roc(labels, preds):
-    # Compute ROC curve and ROC area for each class
-    fpr, tpr, _ = roc_curve(labels.flatten(), preds.flatten())
-    roc_auc = auc(fpr, tpr)
-    return roc_auc
+def main():
+    dataset = 'GALEN'
+    embedding_size = 50
 
+    cls_embeds_file = f'data/{dataset}/classELEmbed_best.pkl'
+    # rel_embeds_file = f'data/{dataset}/relationELEmbed.pkl'
 
-def compute_rank_roc(ranks, n_prots):
-    auc_x = list(ranks.keys())
-    auc_x.sort()
-    auc_y = []
-    tpr = 0
-    sum_rank = sum(ranks.values())
-    for x in auc_x:
-        tpr += ranks[x]
-        auc_y.append(tpr / sum_rank)
-    auc_x.append(n_prots)
-    auc_y.append(1)
-    auc = np.trapz(auc_y, auc_x) / n_prots
-    return auc
+    print('Evaluating')
+    device = get_device()
+
+    cls_df = pd.read_pickle(cls_embeds_file)
+    # rel_df = pd.read_pickle(rel_embeds_file)
+    nb_classes = len(cls_df)
+    # nb_relations = len(rel_df)
+    # print(f'#Classes: {nb_classes}, #Relations: {nb_relations}')
+
+    embeds_list = cls_df['embeddings'].values
+    # classes = {v: k for k, v in enumerate(cls_df['classes'])}
+    # r_embeds_list = rel_df['embeddings'].values
+    # relations = {v: k for k, v in enumerate(rel_df['relations'])}
+    size = len(embeds_list[0])
+    embeds = torch.zeros((nb_classes, size), requires_grad=False).to(device)
+    for i, emb in enumerate(embeds_list):
+        embeds[i, :] = torch.from_numpy(emb).to(device)
+
+    # relations
+    # r_size = len(r_embeds_list[0])
+    # r_embeds = torch.zeros((nb_relations, r_size), requires_grad=False).to(device)
+    # for i, emb in enumerate(r_embeds_list):
+    #     r_embeds[i, :] = torch.from_numpy(emb).to(device)
+
+    print('Loading data')
+    _, classes, relations = load_data(dataset)
+    # train_file = f'data/{dataset}/{dataset}_train.txt'
+    test_file = f'data/{dataset}/{dataset}_test.txt'
+    test_data = load_valid_data(test_file, classes, relations)
+
+    top1, top10, top100, mean_rank, ranks = compute_ranks(embeds, embedding_size, test_data, device, use_tqdm=True)
+
+    ranks_dict = Counter(ranks.tolist())
+    rank_auc = compute_rank_roc(ranks_dict, nb_classes)
+
+    print(
+        f'{dataset}: top1: {top1:.2f}, top10: {top10:.2f}, top100: {top100:.2f}, mean: {mean_rank:.2f}, median: {torch.median(ranks):.2f}, auc: {rank_auc:.2f}')
 
 
 def compute_ranks(embeds, embedding_size, eval_data, device, batch_size=100, use_tqdm=False):
@@ -92,50 +115,19 @@ def compute_ranks(embeds, embedding_size, eval_data, device, batch_size=100, use
     return top1, top10, top100, mean_rank, ranks
 
 
-def main():
-    dataset = 'ANATOMY'
-    embedding_size = 50
-
-    cls_embeds_file = f'data/{dataset}/classELEmbed_best.pkl'
-    # rel_embeds_file = f'data/{dataset}/relationELEmbed.pkl'
-
-    print('Evaluating')
-    device = get_device()
-
-    cls_df = pd.read_pickle(cls_embeds_file)
-    # rel_df = pd.read_pickle(rel_embeds_file)
-    nb_classes = len(cls_df)
-    # nb_relations = len(rel_df)
-    # print(f'#Classes: {nb_classes}, #Relations: {nb_relations}')
-
-    embeds_list = cls_df['embeddings'].values
-    # classes = {v: k for k, v in enumerate(cls_df['classes'])}
-    # r_embeds_list = rel_df['embeddings'].values
-    # relations = {v: k for k, v in enumerate(rel_df['relations'])}
-    size = len(embeds_list[0])
-    embeds = torch.zeros((nb_classes, size), requires_grad=False).to(device)
-    for i, emb in enumerate(embeds_list):
-        embeds[i, :] = torch.from_numpy(emb).to(device)
-
-    # relations
-    # r_size = len(r_embeds_list[0])
-    # r_embeds = torch.zeros((nb_relations, r_size), requires_grad=False).to(device)
-    # for i, emb in enumerate(r_embeds_list):
-    #     r_embeds[i, :] = torch.from_numpy(emb).to(device)
-
-    print('Loading data')
-    _, classes, relations = load_data(dataset)
-    # train_file = f'data/{dataset}/{dataset}_train.txt'
-    test_file = f'data/{dataset}/{dataset}_test.txt'
-    test_data = load_valid_data(test_file, classes, relations)
-
-    top1, top10, top100, mean_rank, ranks = compute_ranks(embeds, embedding_size, test_data, device, use_tqdm=True)
-
-    ranks_dict = Counter(ranks.tolist())
-    rank_auc = compute_rank_roc(ranks_dict, nb_classes)
-
-    print(
-        f'{dataset}: top1: {top1:.2f}, top10: {top10:.2f}, top100: {top100:.2f}, mean: {mean_rank:.2f}, median: {torch.median(ranks):.2f}, auc: {rank_auc:.2f}')
+def compute_rank_roc(ranks, n_prots):
+    auc_x = list(ranks.keys())
+    auc_x.sort()
+    auc_y = []
+    tpr = 0
+    sum_rank = sum(ranks.values())
+    for x in auc_x:
+        tpr += ranks[x]
+        auc_y.append(tpr / sum_rank)
+    auc_x.append(n_prots)
+    auc_y.append(1)
+    auc = np.trapz(auc_y, auc_x) / n_prots
+    return auc
 
 
 if __name__ == '__main__':
