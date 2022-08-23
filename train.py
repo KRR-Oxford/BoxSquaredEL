7da869d363c06b9eaaf81c09a0c61f35d49da2db
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2022-08-23 19:51:31 +0100

    Fixes negative sampling and cleanup

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from model.ElbePlus import ElbePlus
 from model.ElBallModel import ELBallModel
 from model.ELSoftplusBoxModel import ELSoftplusBoxModel
 from model.Elbe import Elbe
-from model.BoxSqEL import BoxSqEL
+from model.BoxSquaredEL import BoxSquaredEL
 from utils.data_loader import DataLoader
 import logging
 import pandas as pd
@@ -28,7 +28,7 @@ def main():
     np.random.seed(seed)
 
     dataset = 'GALEN'
-    task = 'EmELpp'
+    task = 'prediction'
     embedding_dim = 200
 
     wandb.init(project=f"{dataset}-{task}", entity="krr")
@@ -40,11 +40,11 @@ def main():
     val_data = data_loader.load_val_data(dataset, classes)
     val_data['nf1'] = val_data['nf1'][:1000]
     print('Loaded data.')
-    model = Elbe(device, classes, len(relations), embedding_dim, batch=512, margin1=0.05)
+    # model = Elbe(device, classes, len(relations), embedding_dim, batch=512, margin1=0.05)
     # model = ElbePlus(device, classes, len(relations), embedding_dim=embedding_dim, batch=512, margin=0.05,
     #                    disjoint_dist=2, ranking_fn='l2')
-    # model = BoxSqEL(device, classes, len(relations), embedding_dim, batch=512, margin=0.05, disjoint_dist=2,
-    #                 ranking_fn='l2', reg_factor=0.05)
+    model = BoxSquaredEL(device, classes, len(relations), embedding_dim, batch=512, margin=0.05, disjoint_dist=2,
+                         ranking_fn='l2', reg_factor=0.05)
 
     # model = ELSoftplusBoxModel(device, classes, len(relations), embedding_dim=embedding_dim, batch=512, margin=0,
     #                           beta=1, disjoint_dist=2, ranking_fn='softplus')
@@ -54,10 +54,14 @@ def main():
     out_folder = f'data/{dataset}/{task}/{model.name}'
 
     optimizer = optim.Adam(model.parameters(), lr=5e-3)
-    # scheduler = MultiStepLR(optimizer, milestones=[2000], gamma=0.1)
-    scheduler = None
+    scheduler = MultiStepLR(optimizer, milestones=[2000], gamma=0.1)
+    # scheduler = None
     model = model.to(device)
-    train(model, train_data, val_data, len(classes), optimizer, scheduler, out_folder, num_epochs=5000, val_freq=100)
+
+    if not model.negative_sampling and task != 'EmELpp':
+        sample_negatives(train_data)
+
+    train(model, train_data, val_data, len(classes), optimizer, scheduler, out_folder, num_epochs=10000, val_freq=100)
 
     print('Computing test scores...')
     evaluate(dataset, task, model.name, embedding_size=model.embedding_dim, beta=model.beta,
@@ -77,18 +81,10 @@ def train(model, data, val_data, num_classes, optimizer, scheduler, out_folder, 
     try:
         for epoch in trange(num_epochs):
             if model.negative_sampling:
-                nf3 = data['nf3']
-                randoms = np.random.choice(data['prot_ids'], size=(nf3.shape[0], 2))
-                randoms = torch.from_numpy(randoms)
-                new_tails = torch.cat([nf3[:, [0, 1]], randoms[:, 0].reshape(-1, 1)], dim=1)
-                new_heads = torch.cat([randoms[:, 1].reshape(-1, 1), nf3[:, [1, 2]]], dim=1)
-                new_neg = torch.cat([new_tails, new_heads], dim=0)
-                data['nf3_neg'] = new_neg
+                sample_negatives(data)
 
             re = model(data)
             loss = sum(re)
-            if epoch % 1000 == 0:
-                print('epoch:', epoch, 'loss:', round(loss.item(), 3))
             if epoch % val_freq == 0 and val_data is not None:
                 # acc = compute_accuracy(embeds, model.embedding_dim, val_data, model.device)
                 # wandb.log({'acc': acc}, commit=False)
@@ -97,8 +93,8 @@ def train(model, data, val_data, num_classes, optimizer, scheduler, out_folder, 
                 wandb.log({'top10': ranking.top10 / len(ranking), 'top100': ranking.top100 / len(ranking),
                            'mean_rank': np.mean(ranking.ranks), 'median_rank': np.median(ranking.ranks)}, commit=False)
                 # if ranking.top100 >= best_top100:
-                # if np.median(ranking.ranks) <= best_median:
-                if np.mean(ranking.ranks) <= best_mean:
+                if np.median(ranking.ranks) <= best_median:
+                # if np.mean(ranking.ranks) <= best_mean:
                     best_top10 = ranking.top10
                     best_top100 = ranking.top100
                     best_median = np.median(ranking.ranks)
@@ -119,6 +115,16 @@ def train(model, data, val_data, num_classes, optimizer, scheduler, out_folder, 
 
     print(f'Best epoch: {best_epoch}')
     model.save(out_folder)
+
+
+def sample_negatives(data):
+    nf3 = data['nf3']
+    randoms = np.random.choice(data['prot_ids'], size=(nf3.shape[0], 2))
+    randoms = torch.from_numpy(randoms)
+    new_tails = torch.cat([nf3[:, [0, 1]], randoms[:, 0].reshape(-1, 1)], dim=1)
+    new_heads = torch.cat([randoms[:, 1].reshape(-1, 1), nf3[:, [1, 2]]], dim=1)
+    new_neg = torch.cat([new_tails, new_heads], dim=0)
+    data['nf3_neg'] = new_neg
 
 
 if __name__ == '__main__':
