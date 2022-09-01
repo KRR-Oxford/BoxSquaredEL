ed6ed3eb31d47cee0563d7300c3e5c1cee0ee21b
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2022-09-01 12:26:58 +0100

    Small cleanup

--- train.py
+++ train.py
@@ -46,11 +46,10 @@ def run(use_wandb=True):
     val_data = data_loader.load_val_data(dataset, classes)
     val_data['nf1'] = val_data['nf1'][:1000]
     print('Loaded data.')
-    model = Elem(device, classes, len(relations), embedding_dim, margin=0.05)
+    # model = Elem(device, classes, len(relations), embedding_dim, margin=0.05)
     # model = Elbe(device, classes, len(relations), embedding_dim, margin1=0.05)
     # model = ElbePlus(device, classes, len(relations), embedding_dim=embedding_dim, margin=0.05, neg_dist=2)
-    # model = BoxSquaredEL(device, classes, len(relations), embedding_dim, margin=0.05, neg_dist=2,
-    #                      reg_factor=0.05, num_neg=num_neg)
+    model = BoxSquaredEL(device, classes, len(relations), embedding_dim, margin=0.05, neg_dist=2, reg_factor=0.05, num_neg=num_neg)
 
     out_folder = f'data/{dataset}/{task}/{model.name}'
 
